f425e8d0be34cf75f3398a8fda571aa412b8c938
Benjamin Liles <benliles@gmail.com>
2011-04-18 13:35:19 -0500

    Added debug logging to middleware and some documentation

--- proxiedssl/middleware.py
+++ proxiedssl/middleware.py
@@ -1,10 +1,21 @@
+from logging import getLogger
+
+
+
+log = getLogger('proxiedssl')
+
 class ProxiedSslWsgiMiddleware(object):
-    
     def process_request(self, request):
-        print str(request.META)
         if 'HTTP_X_URL_SCHEME' in request.META:
+            log.debug('setting wsgi.url_scheme to %s' % 
+                      (request.META['HTTP_X_URL_SCHEME'].lower(),))
             request.environ['wsgi.url_scheme'] = \
                 request.META['HTTP_X_URL_SCHEME'].lower()
         elif 'HTTP_X_FORWARDED_PROTOCOL' in request.META:
+            log.debug('setting wsgi.url_scheme to %s' % 
+                      (request.META['HTTP_X_FORWARDED_PROTOCOL'].lower(),))
             request.environ['wsgi.url_scheme'] = \
                 request.META['HTTP_X_FORWARDED_PROTOCOL'].lower()
+        else:
+            return
+        log.debug('request.is_secure = %s' % (request.is_secure(),))
